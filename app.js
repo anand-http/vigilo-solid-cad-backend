0d1190971516617d7f1893c8fc6a2632547eba2e
Anand <rishusingh9369@gmail.com>
2025-05-07 12:47:02 +0530

    cors issue

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ app.use(express.json());
 
 
 
-const corsOptions = {
-    origin: "*",
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-};
-
-app.use(cors(corsOptions));
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+        credentials: true,
+    })
+);
 
 
 app.get('/', (req, res) => {
